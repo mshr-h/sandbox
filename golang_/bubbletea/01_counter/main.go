8523d8fd61c01862dd6516ae4f5ab4e6c89c9e05
Masahiro Hiramori <mhg00g13@gmail.com>
2022-07-13 08:25:51 +0900

    add bubbletea counter example

---
+++ golang_/bubbletea/01_counter/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	count int
+}
+
+func (model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "a":
+			m.count = m.count + 1
+		case "x":
+			m.count = m.count - 1
+			return m, nil
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("a: increment, x: decrement, q, ctrl+c: exit\n  count: %v", m.count)
+}
+
+var _ tea.Model = (*model)(nil)
+
+func main() {
+	prog := tea.NewProgram((model{count: 0}))
+	err := prog.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
